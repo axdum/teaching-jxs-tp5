44975038d26db6e2560ee17de1911e968277462f
axdum <axel.dumont3@gmail.com>
2018-03-09 23:18:03 +0100

    - Q10 OK

--- js/pokedex.js
+++ js/pokedex.js
@@ -3,6 +3,32 @@ var pokeApp = angular.module('pokedex', ['ngResource']);
 // With this you can inject POKEAPI url wherever you want
 pokeApp.constant('POKEAPI', 'http://pokeapi.co');
 
-pokeApp.config(['$resourceProvider', function($resourceProvider) {
+pokeApp.config(['$resourceProvider', function ($resourceProvider) {
     $resourceProvider.defaults.stripTrailingSlashes = false;
 }]);
+
+pokeApp.controller('poke', function ($scope, $log, $http) {
+    /*
+    $scope.pokemonslist = [
+        {id : 0, name : "Bulbizarre" },
+        {id : 1, name : "Herbizarre" },
+        {id : 2, name : "Florizarre" },
+        {id : 3, name : "Salameche" },
+        {id : 4, name : "Reptincel" },
+        {id : 5, name : "Dracaufeu" },
+        {id : 6, name : "Carapuce" },
+        {id : 7, name : "Carabaffe" },
+        {id : 8, name : "Tortank" }
+    ];*/
+    $scope.$log = $log;
+    $http.get('https://pokeapi.co/api/v2/pokemon/?limit=950').then(function success(response) {
+        $scope.pokemonslist = response.data.results;
+    }, function error(response) {
+        console.log(response.message);
+    });
+});
+
+pokeApp.factory('getPokeInfos',  ['$resource',function($resource){
+    return $resource('https://pokeapi.co/api/v2/pokemon/:id');
+}]);
+
