b50f09006ed7be71bcbd01cf4466aac649c099a9
axdum <axel.dumont3@gmail.com>
2018-03-21 23:34:15 +0100

    - Q15 OK

--- js/pokedex.js
+++ js/pokedex.js
@@ -1,56 +1,81 @@
 var pokeApp = angular.module('pokedex', ['ngResource']);
 
-// With this you can inject POKEAPI url wherever you want
-pokeApp.constant('POKEAPI', 'http://pokeapi.co');
+// With this you can inject POKEAPI url wherever you want.
+pokeApp.constant('POKEAPI', 'https://pokeapi.co');
 
 // Services
-pokeApp.factory('getPokeInfosById',  function($resource){
-	return $resource('https://pokeapi.co/api/v2/pokemon/:id');
+// Get Poke by Id.
+/*pokeApp.factory('getPokeInfosById',  function($resource, POKEAPI){
+	return $resource(POKEAPI + '/api/v2/pokemon/:id');
+});*/
+
+// Get Poke by Name.
+pokeApp.factory('getPokeInfosByName',  function($resource, POKEAPI){
+	return $resource(POKEAPI + '/api/v2/pokemon/:name');
 });
 
-pokeApp.factory('getPokeInfosByName',  function($resource){
-	return $resource('https://pokeapi.co/api/v2/pokemon/:name');
-});
-
+// Shared service, pokemon to search .
 pokeApp.factory('SharedService', function() {
-	var service = { id:'1',name: 'bulbasaur'};
+	var service = { pokemon:'' };
 	return service;
 });
 
-pokeApp.controller('poke', function ($scope, $log, $http, SharedService) {
-	$scope.$log = $log;
-	$http.get('https://pokeapi.co/api/v2/pokemon/?limit=50').then(function success(response) {
-		console.log('Pokémons chargés !');
-		$scope.pokemonslist = response.data.results;
-	}, function error(response) {
-		console.log(response.message);
-	});
-
-	$scope.update=function(name){
-		SharedService.name=name;
-		console.log('SharedService update :' + name);
+// Directive
+// Include pokedex.html.
+pokeApp.directive('pokedex', function(){
+	return{
+		template: '<div ng-include="\'pokedex.html\'"></div>'
 	}
 });
 
-pokeApp.controller('details', function ($scope, $log, getPokeInfosByName, SharedService){
-	function search(){
-		console.log('Search :' + SharedService.name);
-		$scope.img = './img/pok.gif';
-		getPokeInfosByName.get({name: SharedService.name}, function(data){
-			console.log('Pokémon trouvé !');
-			$scope.id = data.id;
-			$scope.name = data.name;
-			$scope.img = data.sprites.front_default;
-			$scope.abilities = data.abilities;
-			$scope.stats = data.stats;
-			$scope.weight = Math.round((data.weight*0.1) * 10 ) / 10;
-			$scope.height = Math.round((data.height*0.1) * 10 ) / 10;
-			$scope.moves = data.moves;
-			$scope.exp = data.base_experience;
-			$scope.types = data.types;
-		}, function(){
-			console.log('Pokémon non trouvé !');
+// Poke Controller: search pokemon.
+pokeApp.controller('poke', function ($scope, $log, $http,getPokeInfosByName, SharedService, POKEAPI) {
+
+	function getPokemonInfos(){
+		$scope.pokemon = getPokeInfosByName.get({name: $scope.selectedPokemon}, function(){
+			SharedService.pokemon = $scope.pokemon;
 		});
-	};
-	$scope.$watch(SharedService.name, search);
+	}
+
+	$scope.$log = $log;
+	$scope.selectedPokemon = 'bulbasaur';
+
+	// load bulbasaur
+	getPokemonInfos();
+
+	// Load pokemonlist : modifier la limite pour charger tous les pokemons !
+	$http.get(POKEAPI + '/api/v2/pokemon/?limit=949').then(function success(response) {
+		console.log('Pokémons chargés !');
+		$scope.pokemonslist = response.data.results;
+	}, function error(response) {
+		$log.log(response.message);
+	});
+
+	$scope.go=function(){
+		getPokemonInfos();
+	}
+});
+
+// Details Controller: display pokemon infos.
+pokeApp.controller('details', function ($scope, $log, SharedService){
+	$scope.img = './img/pok.gif';
+	$scope.SharedService = SharedService;
+
+	function displayDetails(poke) {
+		$scope.id = poke.id;
+		$scope.name = poke.name;
+		$scope.img = poke.sprites.front_default;
+		$scope.abilities = poke.abilities;
+		$scope.stats = poke.stats;
+		$scope.weight = Math.round((poke.weight*0.1) * 10 ) / 10;
+		$scope.height = Math.round((poke.height*0.1) * 10 ) / 10;
+		$scope.moves = poke.moves;
+		$scope.exp = poke.base_experience;
+		$scope.types = poke.types;
+	}
+
+	$scope.$watch('SharedService.pokemon', function(newVal){
+		displayDetails(newVal);
+		$log.log('Details de ' + newVal.name + ' chargés !');
+	});
 });
